e486a4399a6935b23839f0038b7912e72adca971
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 00:36:28 +0600

    Hello javascript ph-8

---
+++ math.js
@@ -0,0 +1,19 @@
+/***
+ * 1. number ==>55,898, -32
+ * 2. integer: 2, 5,
+ * 3. float: decimal: 2.5, 13.67, 32.69
+ * 4. operation: addition, substraction, multipication, division, remainder
+ * 5. percent,
+ * 6. average
+ * 7. comparison:v >, <.  ==, !=, >=, <=
+ * 8. oikik: 2 kilo apple and 5 kilo mango:
+ * 9. 45, 25, 68, 68, 129, 2: min, average, sun
+ * 10. series:
+ * ::: EXTRA: :::
+ * 1. prime
+ * 2. permutation
+ * 3. area:
+ * :::: ALWAYS::::
+ *  Jodi lage ami kuje bahir korbo.....\
+ * search on google and youtube
+ **/ 
---
+++ variable.js
@@ -0,0 +1,21 @@
+// vary + able = variable
+
+// numeric
+var price = 13;
+var price = 11;
+var age = 17;
+var temperature = 38;
+var weight = 65;
+var money = 1500;
+var friend = 3;
+
+// string
+var person = "sodor uddin" ;
+var location = "andor killa bandorbon" ;
+var special = 'alia bhat' ;
+
+// Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
+
